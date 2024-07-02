05986263e0ba7c935d3f7d267ab3e93903942444
Chase C <13863948+ChaseCares@users.noreply.github.com>
2024-07-02 04:05:32 -0400

    Mild sauce improvement

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,61 @@ fn update_helper(ui: MainWindow, mut app_state: AppState) {
         app_state.add_to_log("Already checking for updates...");
         ui.set_log(app_state.log.clone().into());
         return;
-    } else {
-        ui.set_checking(true);
-        app_state.add_to_log("Checking for updates...");
-        ui.set_log(app_state.log.clone().into());
     }
 
-    let current_version = env!("CARGO_PKG_VERSION");
-    app_state.add_to_log(format!("Current version: v{}", current_version).as_str());
+    ui.set_checking(true);
+    app_state.add_to_log("Checking for updates...");
     ui.set_log(app_state.log.clone().into());
 
-    let status = self_update::backends::github::Update::configure()
-        .repo_owner("ChaseCares")
+    let current_version = env!("CARGO_PKG_VERSION");
+    app_state.add_to_log(&format!("Current version: v{}", current_version));
+    ui.set_log(app_state.log.clone().into());
+
+    let status = match self_update::backends::github::Update::configure()
+        .repo_owner("repo_owner")
         .repo_name("test")
         .bin_name("test")
         .bin_path_in_archive("{{ bin }}-{{ version }}-{{ target }}/{{ bin }}")
         .show_download_progress(true)
         .current_version(current_version)
         .build()
-        .unwrap();
+    {
+        Ok(status) => status,
+        Err(e) => {
+            app_state.add_to_log(&format!("Error configuring update: {}", e));
+            ui.set_log(app_state.log.clone().into());
+            ui.set_checking(false);
+            return;
+        }
+    };
 
-    let latest = status.get_latest_release().unwrap();
+    let latest = match status.get_latest_release() {
+        Ok(latest) => latest,
+        Err(e) => {
+            app_state.add_to_log(&format!("Error fetching latest release: {}", e));
+            ui.set_log(app_state.log.clone().into());
+            ui.set_checking(false);
+            return;
+        }
+    };
 
-    if self_update::version::bump_is_greater(current_version, &latest.version).unwrap() {
+    if self_update::version::bump_is_greater(current_version, &latest.version).unwrap_or(false) {
         println!(
-            "New update Available: v{}, current_version: v{current_version}",
-            latest.version
+            "New update available: v{}, current version: v{}",
+            latest.version, current_version
         );
-        app_state.add_to_log(format!("New update Available: v{}", latest.version).as_str());
-        ui.set_log(app_state.log.clone().into());
-
+        app_state.add_to_log(&format!("New update available: v{}", latest.version));
         ui.set_ver(latest.version.into());
     } else if current_version == latest.version {
         println!("You are already using the latest version.");
         app_state.add_to_log("You are already using the latest version.");
-        ui.set_log(app_state.log.clone().into());
     } else {
         println!("You are using a newer version than the latest.");
         app_state.add_to_log("You are using a newer version than the latest.");
-        ui.set_log(app_state.log.clone().into());
     }
+
+    ui.set_log(app_state.log.clone().into());
+    ui.set_checking(false);
 }
 
 #[derive(Default, Clone)]
@@ -76,15 +91,9 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.set_log(app_state.log.as_str().into());
 
     ui.on_check_update(move || {
-        update_helper(ui_weak.upgrade().unwrap(), app_state.clone());
-    });
-
-    ui.on_self_update(move || {
-        app_state.self_update = true;
-        ui_weak
-            .upgrade()
-            .unwrap()
-            .set_self_updateing(app_state.self_update);
+        if let Some(ui) = ui_weak.upgrade() {
+            update_helper(ui, app_state.clone());
+        }
     });
 
     ui.run()
